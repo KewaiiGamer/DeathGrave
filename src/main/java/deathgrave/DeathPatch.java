947211256ef34adc96950a3b26f371c1ae44a54b
KewaiiGamer <miguelviegas1999@gmail.com>
2022-07-07 17:33:45 +0100

    Prevent Death Grave from being placed on Spawn or on top of other objects.

--- src/main/java/deathgrave/DeathPatch.java
+++ src/main/java/deathgrave/DeathPatch.java
@@ -2,17 +2,16 @@ package deathgrave;
 
 import deathgrave.inventory.DeathGrave;
 import necesse.engine.modLoader.annotations.ModMethodPatch;
+import necesse.engine.network.server.ServerClient;
 import necesse.engine.registries.ObjectRegistry;
 import necesse.entity.mobs.Attacker;
 import necesse.entity.mobs.Mob;
 import necesse.entity.mobs.PlayerMob;
-import necesse.entity.objectEntity.InventoryObjectEntity;
 import necesse.entity.pickup.ItemPickupEntity;
 import necesse.entity.pickup.PickupEntity;
 import net.bytebuddy.asm.Advice;
 
 import java.util.HashSet;
-import java.util.Iterator;
 
 @ModMethodPatch(
         target = PlayerMob.class,
@@ -25,15 +24,25 @@ public class DeathPatch {
         DeathGrave deathGrave = (DeathGrave) ObjectRegistry.getObject("kew_grave");
         int tileX = mob.getTileX();
         int tileY = mob.getTileY();
+        ServerClient serverClient = ((PlayerMob) mob).getServerClient();
+        boolean changeCoord = true;
+        if (tileX == serverClient.spawnTile.x && tileY == serverClient.spawnTile.y) {
+            tileY++;
+        }
+        while (deathGrave.canPlace(mob.getLevel(), tileX, tileY, 0) != null) {
+            if (changeCoord) tileX++;
+            else tileY++;
+            changeCoord = !changeCoord;
+        }
         deathGrave.placeObject(mob.getLevel(), tileX, tileY, 0);
-        DeathGrave.DeathGraveInventoryObjectEntity objectEntity = (DeathGrave.DeathGraveInventoryObjectEntity)mob.getLevel().entityManager.getObjectEntity(tileX, tileY);
+        DeathGrave.DeathGraveInventoryObjectEntity objectEntity = (DeathGrave.DeathGraveInventoryObjectEntity) mob.getLevel().entityManager.getObjectEntity(tileX, tileY);
 
         for (PickupEntity pickupEntity : mob.getLevel().entityManager.pickups) {
             ItemPickupEntity next = (ItemPickupEntity) pickupEntity;
             objectEntity.getInventory().addItem(mob.getLevel(), null, next.item, "death");
         }
 
-        deathGrave.setServerClient(((PlayerMob)mob).getServerClient());
+        deathGrave.setServerClient(serverClient);
         mob.getLevel().sendObjectUpdatePacket(tileX, tileY);
     }
 }
